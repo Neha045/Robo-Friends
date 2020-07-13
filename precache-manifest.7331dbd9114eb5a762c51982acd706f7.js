02cbb5e3ba5f07013fcd0d7d4ec0115ed02ec427
Neha Sonpure <sonpureneha@gmail.com>
2020-07-13 17:48:18 +0530

    Updates

---
+++ precache-manifest.7331dbd9114eb5a762c51982acd706f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f48e88fc6981197baad9765940757df",
+    "url": "/Neha045/Robo-Friends/index.html"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/Neha045/Robo-Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5166e11cb9bc6cfb32ca",
+    "url": "/Neha045/Robo-Friends/static/css/main.29753e3e.chunk.css"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/Neha045/Robo-Friends/static/js/2.be3d66de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Neha045/Robo-Friends/static/js/2.be3d66de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5166e11cb9bc6cfb32ca",
+    "url": "/Neha045/Robo-Friends/static/js/main.d8640a6b.chunk.js"
+  },
+  {
+    "revision": "fe09ca72eb727c4d2f11",
+    "url": "/Neha045/Robo-Friends/static/js/runtime-main.65b6bd9b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Neha045/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Neha045/Robo-Friends/precache-manifest.15ad28b7baa76c5a89e310026259922b.js"
+  "/Neha045/Robo-Friends/precache-manifest.7331dbd9114eb5a762c51982acd706f7.js"
 );
 
 self.addEventListener('message', (event) => {
